481a6214f5221cdbbcfd96d9437880fab6a96e60
Oro <Oro@DESKTOP-UKRN5OT>
2019-05-26 09:50:11 +0200

    V.2 Imprimimos - "Prueba para el examen V.2"

--- PruebaED/src/Examen.java
+++ PruebaED/src/Examen.java
@@ -4,6 +4,7 @@ public class Examen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub 
         System.out.println("Prueba para el examen");
+        System.out.println("Prueba para el examen V.2");
 	}
 
 }
